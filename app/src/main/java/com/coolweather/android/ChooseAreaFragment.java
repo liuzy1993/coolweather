63f7da0957475ec1c74ae150b36692e252d2d623
liuzy <d_liuzhiyuan@163.com>
2017-03-10 11:12:29 +0800

    测试修改

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -197,7 +197,7 @@ public class ChooseAreaFragment extends Fragment {
                     @Override
                     public void run() {
                         closeProgressDialog();
-                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(),"加载很失败",Toast.LENGTH_SHORT).show();
                     }
                 });
             }
